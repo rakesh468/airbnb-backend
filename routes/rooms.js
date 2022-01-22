22c49b1e3a085f5c07b1676089c277e34dbf0c8e
rakesh468 <rakeshkumarg2000@gmail.com>
2022-01-22 02:25:13 -0800

    first  commit

--- routes/rooms.js
+++ routes/rooms.js
@@ -31,7 +31,7 @@ router
     const aribnb = await GetroomsById(id);
     response.send(aribnb);
   })
-  .delete(auth,async (request, response) => {
+  .delete(async (request, response) => {
     const { id } = request.params;
     const airbnb = await DeleteRoomById(id);
     airbnb.deletedCount > 0
@@ -39,7 +39,7 @@ router
       : response.status(401).send({ message: "Page not found" });
   }) //delete room by id using delete method//
 
-  .put(auth,async (request, response) => {
+  .put(async (request, response) => {
     const { id } = request.params;
     const data = request.body;
     const result = await UpdateRoomsById(id, data);
