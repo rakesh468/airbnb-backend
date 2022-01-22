212537211eb5067e3bb260596f2a019f4ec0d051
rakesh468 <rakeshkumarg2000@gmail.com>
2022-01-22 00:37:19 -0800

    first commit

--- routes/rooms.js
+++ routes/rooms.js
@@ -18,7 +18,7 @@ router
     const result = await CreateRooms(data);
     response.send(result);
   }) //adding rooms using post method//
-  .get(auth, async (request, response) => {
+  .get(async (request, response) => {
     const data = await Getrooms();
     response.send(data);
   }); //getting all rooms using get method//
